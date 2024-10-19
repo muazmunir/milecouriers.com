ca4ac5fa0aa7675f560ad37dabf51a31b2f4f5bc
Muaz Munir <muazmuneer151m@gmail.com>
2024-10-19 15:25:29 +0500

    v4

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,10 +5,16 @@ namespace App\Providers;
 use App\Interfaces\DeliveryTimeInterface;
 use App\Interfaces\PaymentMethodInterface;
 use App\Interfaces\RoleInterface;
+use App\Interfaces\ServiceModeInterface;
+use App\Interfaces\ShippingModeInterface;
+use App\Interfaces\TypesOfPackingInterface;
 use App\Interfaces\UserInterface;
 use App\Repositories\DeliveryTimeRepository;
 use App\Repositories\PaymentMethodRepository;
 use App\Repositories\RoleRepository;
+use App\Repositories\ServiceModeRepository;
+use App\Repositories\ShippingModeRepository;
+use App\Repositories\TypesOfPackingRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,5 +26,8 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(RoleInterface::class, RoleRepository::class);
         $this->app->bind(DeliveryTimeInterface::class, DeliveryTimeRepository::class);
         $this->app->bind(PaymentMethodInterface::class, PaymentMethodRepository::class);
+        $this->app->bind(ShippingModeInterface::class, ShippingModeRepository::class);
+        $this->app->bind(TypesOfPackingInterface::class, TypesOfPackingRepository::class);
+        $this->app->bind(ServiceModeInterface::class, ServiceModeRepository::class);
     }
 }
