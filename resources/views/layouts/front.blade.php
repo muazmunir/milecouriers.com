faece1f4d5fd7e90a6404d0af7ced4acb76b4376
Muaz Munir <muazmuneer151m@gmail.com>
2024-11-26 14:10:47 +0500

    text updated

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -72,9 +72,9 @@
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Address</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>LG-69, Zainab Tower Link Road, Lahore</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+92 304 0110261</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>milecourierservice@gmail.com</p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
